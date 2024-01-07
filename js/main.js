75244d9286722504df0af060f83812f6324e6bac
Abdiahman Ahmed <xarago125@gmail.com>
2024-01-07 21:08:38 +0300

    adding js in nav bar

--- js/main.js
+++ js/main.js
@@ -9,4 +9,14 @@ $(document).ready(function () {
     $(this).toggleClass("fa-times");
     $(".navbar").toggleClass("nav-toggle");
   });
+
+  $(window).on("scroll load", function () {
+    $(".fa-bars").removeClass("fa-times");
+    $(".navbar").removeClass("nav-toggle");
+
+    if ($(window).scrollTop() > 30) {
+      $("header").addClass("header-active");
+    } else {
+    }
+  });
 });
